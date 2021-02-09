6cf4e43b0b0f69fa6913ae5f56f4e6fb453ec5ba
Visnaa <visnaayt@gmail.com>
2021-02-09 10:38:33 +0100

    Fixed tab name

--- src/main/java/com/visnaa/morejewels/Main.java
+++ src/main/java/com/visnaa/morejewels/Main.java
@@ -18,7 +18,7 @@ import org.apache.logging.log4j.Logger;
 public class Main
 {
     public static final Logger LOGGER = LogManager.getLogger();
-    public static final ItemGroup ITEM_GROUP = new MoreJewelsTab("item_group");
+    public static final ItemGroup ITEM_GROUP = new MoreJewelsTab("morejewels_tab");
 
     public Main() {
         IEventBus bus = FMLJavaModLoadingContext.get().getModEventBus();
