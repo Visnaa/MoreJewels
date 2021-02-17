211f7cc1428e65bf828d8d15107e67131680ea5d
Visnaa <visnaayt@gmail.com>
2021-02-17 10:58:07 +0100

    Updated ore generation system

--- src/main/java/com/visnaa/morejewels/world/CustomOreGen.java
+++ src/main/java/com/visnaa/morejewels/world/CustomOreGen.java
@@ -27,73 +27,73 @@ public class CustomOreGen
         overworldOres.add(register("ruby_ore", Feature.ORE.withConfiguration(new OreFeatureConfig(
                 OreFeatureConfig.FillerBlockType.BASE_STONE_OVERWORLD, ModBlocks.RUBY_ORE.get().getDefaultState(), 4))
                 .range(40).square()
-                .func_242731_b(16)));
+                .func_242731_b( 8)));
         overworldOres.add(register("sapphire_ore", Feature.ORE.withConfiguration(new OreFeatureConfig(
                 OreFeatureConfig.FillerBlockType.BASE_STONE_OVERWORLD, ModBlocks.SAPPHIRE_ORE.get().getDefaultState(), 4))
                 .range(40).square()
-                .func_242731_b(16)));
+                .func_242731_b( 8)));
         overworldOres.add(register("aquamarine_ore", Feature.ORE.withConfiguration(new OreFeatureConfig(
                 OreFeatureConfig.FillerBlockType.BASE_STONE_OVERWORLD, ModBlocks.AQUAMARINE_ORE.get().getDefaultState(), 4))
                 .range(40).square()
-                .func_242731_b(16)));
+                .func_242731_b( 8)));
         overworldOres.add(register("jade_ore", Feature.ORE.withConfiguration(new OreFeatureConfig(
                 OreFeatureConfig.FillerBlockType.BASE_STONE_OVERWORLD, ModBlocks.JADE_ORE.get().getDefaultState(), 4))
                 .range(40).square()
-                .func_242731_b(16)));
+                .func_242731_b( 8)));
         overworldOres.add(register("opal_ore", Feature.ORE.withConfiguration(new OreFeatureConfig(
                 OreFeatureConfig.FillerBlockType.BASE_STONE_OVERWORLD, ModBlocks.OPAL_ORE.get().getDefaultState(), 4))
                 .range(40).square()
-                .func_242731_b(16)));
+                .func_242731_b( 8)));
         overworldOres.add(register("yellow_diamond_ore", Feature.ORE.withConfiguration(new OreFeatureConfig(
                 OreFeatureConfig.FillerBlockType.BASE_STONE_OVERWORLD, ModBlocks.YELLOW_DIAMOND_ORE.get().getDefaultState(), 4))
                 .range(40).square()
-                .func_242731_b(16)));
+                .func_242731_b( 8)));
         overworldOres.add(register("amber_ore", Feature.ORE.withConfiguration(new OreFeatureConfig(
                 OreFeatureConfig.FillerBlockType.BASE_STONE_OVERWORLD, ModBlocks.AMBER_ORE.get().getDefaultState(), 4))
                 .range(40).square()
-                .func_242731_b(16)));
+                .func_242731_b( 8)));
         overworldOres.add(register("amethyst_ore", Feature.ORE.withConfiguration(new OreFeatureConfig(
                 OreFeatureConfig.FillerBlockType.BASE_STONE_OVERWORLD, ModBlocks.AMETHYST_ORE.get().getDefaultState(), 4))
                 .range(40).square()
-                .func_242731_b(16)));
+                .func_242731_b( 8)));
         overworldOres.add(register("topaz_ore", Feature.ORE.withConfiguration(new OreFeatureConfig(
                 OreFeatureConfig.FillerBlockType.BASE_STONE_OVERWORLD, ModBlocks.TOPAZ_ORE.get().getDefaultState(), 4))
                 .range(40).square()
-                .func_242731_b(16)));
+                .func_242731_b( 8)));
         overworldOres.add(register("platinum_ore", Feature.ORE.withConfiguration(new OreFeatureConfig(
                 OreFeatureConfig.FillerBlockType.BASE_STONE_OVERWORLD, ModBlocks.PLATINUM_ORE.get().getDefaultState(), 4))
                 .range(40).square()
-                .func_242731_b(16)));
+                .func_242731_b( 8)));
         overworldOres.add(register("baryllium_ore", Feature.ORE.withConfiguration(new OreFeatureConfig(
                 OreFeatureConfig.FillerBlockType.BASE_STONE_OVERWORLD, ModBlocks.BERYLLIUM_ORE.get().getDefaultState(), 4))
                 .range(40).square()
-                .func_242731_b(16)));
+                .func_242731_b( 8)));
         overworldOres.add(register("bixbit_ore", Feature.ORE.withConfiguration(new OreFeatureConfig(
                 OreFeatureConfig.FillerBlockType.BASE_STONE_OVERWORLD, ModBlocks.BIXBIT_ORE.get().getDefaultState(), 4))
                 .range(40).square()
-                .func_242731_b(16)));
+                .func_242731_b( 8)));
         overworldOres.add(register("malachite_ore", Feature.ORE.withConfiguration(new OreFeatureConfig(
                 OreFeatureConfig.FillerBlockType.BASE_STONE_OVERWORLD, ModBlocks.MALACHITE_ORE.get().getDefaultState(), 4))
                 .range(40).square()
-                .func_242731_b(16)));
+                .func_242731_b( 8)));
         overworldOres.add(register("onyx_ore", Feature.ORE.withConfiguration(new OreFeatureConfig(
                 OreFeatureConfig.FillerBlockType.BASE_STONE_OVERWORLD, ModBlocks.ONYX_ORE.get().getDefaultState(), 4))
                 .range(40).square()
-                .func_242731_b(16)));
+                .func_242731_b( 8)));
         overworldOres.add(register("peridot_ore", Feature.ORE.withConfiguration(new OreFeatureConfig(
                 OreFeatureConfig.FillerBlockType.BASE_STONE_OVERWORLD, ModBlocks.PERIDOT_ORE.get().getDefaultState(), 4))
                 .range(40).square()
-                .func_242731_b(16)));
+                .func_242731_b( 8)));
         
         //netherOres.add(register("", Feature.ORE.withConfiguration(new OreFeatureConfig(
         //        OreFeatureConfig.FillerBlockType.NETHERRACK, ModBlocks."BLOCK_NAME".get().getDefaultState(), 4))
         //        .range(48).square()
-        //        .func_242731_b(64)));
+        //        .func_242731_b(84)));
         //
         //endOres.add(register("", Feature.ORE.withConfiguration(new OreFeatureConfig(
         //        new BlockMatchRuleTest(Blocks.END_STONE), ModBlocks."BLOCK NAME".get().getDefaultState(), 4))
         //        .range(128).square()
-        //        .func_242731_b(64)));
+        //        .func_242731_b(84)));
     }
 
     @SubscribeEvent(priority = EventPriority.HIGHEST)
